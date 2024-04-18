06d9b7065a88cfa39123d099333406754aefe3d5
Ariyaman Debnath <ariyamandebnath.ad@gmail.com>
2024-04-18 23:10:40 +0530

    routers added

---
+++ backend/src/routes/resturant.routes.ts
@@ -0,0 +1,73 @@
+import express from "express";
+import multer from "multer";
+import MyRestaurantController from "../controllers/resturant.controller";
+import { jwtCheck, jwtParse } from "../middleware/auth.middleware";
+import { validateMyResturantRequest } from "../middleware/validation.middleware";
+import { param } from "express-validator";
+
+const router = express.Router();
+
+const storage = multer.memoryStorage();
+const upload = multer({
+  storage: storage,
+  limits: {
+    fileSize: 5 * 1024 * 1024, //5mb
+  },
+});
+
+router.get(
+  "/order",
+  jwtCheck,
+  jwtParse,
+  MyRestaurantController.getMyRestaurantOrders
+);
+
+router.patch(
+  "/order/:orderId/status",
+  jwtCheck,
+  jwtParse,
+  MyRestaurantController.updateOrderStatus
+);
+
+router.get("/", jwtCheck, jwtParse, MyRestaurantController.getMyRestaurant);
+
+router.post(
+  "/",
+  upload.single("imageFile"),
+  validateMyResturantRequest,
+  jwtCheck,
+  jwtParse,
+  MyRestaurantController.createMyRestaurant
+);
+
+router.put(
+  "/",
+  upload.single("imageFile"),
+  validateMyResturantRequest,
+  jwtCheck,
+  jwtParse,
+  MyRestaurantController.updateMyRestaurant
+);
+
+
+router.get(
+  "/:restaurantId",
+  param("restaurantId")
+    .isString()
+    .trim()
+    .notEmpty()
+    .withMessage("RestaurantId paramenter must be a valid string"),
+  MyRestaurantController.getRestaurant
+);
+
+router.get(
+  "/search/:city",
+  param("city")
+    .isString()
+    .trim()
+    .notEmpty()
+    .withMessage("City paramenter must be a valid string"),
+  MyRestaurantController.searchRestaurant
+);
+
+export default router;
